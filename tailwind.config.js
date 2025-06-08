a4f50e0d4c4d7eeaf1d59e9bcf8d6ea6bf15339e
AhsanRosikhanYusri <ahsanbelajar2020@gmail.com>
2025-06-08 17:29:52 +0700

    feat: demo morque

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,css,js}"],
   theme: {
     extend: {
-      fontFamily: {
-         tilt: ['"Tilt Warp"', 'cursive'],
-        lexend: ['"Lexend"', 'sans-serif'],
-        urbanist: ['"Urbanist"', 'sans-serif'],
-      }
+ 
     },
   },
   plugins: [],
-} 
+}
